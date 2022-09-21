abb4304d87a0a45b5cf0fbb1988ea7ed72296b1a
Matías Scavuzzo <mati.ntvg@hotmail.com>
2022-09-20 21:38:56 -0300

    Greeting

--- react-app/src/components/ItemListContainer.js
+++ react-app/src/components/ItemListContainer.js
@@ -1,9 +1,9 @@
 const ItemListContainer = (props) => {
     return (
     <div className={props.className}>
-        <a className='z-0 p-4 hover:bg-[#f7dcca] hover:text-black' href="index.html">Item 1</a>
-        <a className='z-10 p-4 hover:bg-[#f7dcca] hover:text-black' href="index.html">Item 2</a>
-        <a className='z-20 p-4 hover:rounded-b-md hover:bg-[#f7dcca] hover:text-black' href="index.html">Item 3</a>
+        <a className='z-0 p-4 hover:bg-[#f7dcca] hover:text-black' href="index.html">{props.greeting}</a>
+        <a className='z-10 p-4 hover:bg-[#f7dcca] hover:text-black' href="index.html">{props.greeting}</a>
+        <a className='z-20 p-4 hover:rounded-b-md hover:bg-[#f7dcca] hover:text-black' href="index.html">{props.greeting}</a>
     </div>
     )
 }
--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -16,21 +16,21 @@ const Navbar = ({className}) => {
     return (
       <div className={className}>
         <nav className="flex items-center justify-end w-full text-white">
-          <div className={`${ menuMovil ? 'hidden' : 'flex'} absolute shadow-2xl slide-bottom shadow-black rounded-lg sm:relative sm:top-0 sm:z-10 p-2 flex-col items-center justify-center text-right w-full left-0 bg-black -z-10 opacity-95 top-[18vw] sm:flex sm:flex-row`}>
+          <div className={`${ menuMovil ? 'hidden' : 'flex'} absolute shadow-2xl slide-bottom shadow-black rounded-lg sm:relative sm:top-0 sm:z-10 p-2 flex-col items-center justify-center text-right w-full left-0 bg-black -z-10 opacity-95 sm:opacity-100 top-[18vw] sm:flex sm:flex-row`}>
               <a href="index.html" className='w-full sm:text-center'>Tienda</a>
-              <div className='w-full flex flex-col my-2 sm:m-0 sm:items-center'>
-                <button onClick={toggleMenu} type='button' className='flex relative flex-row items-end justify-end'>
+              <div className='flex flex-col w-full my-2 sm:m-0 sm:items-center'>
+                <button onClick={toggleMenu} type='button' className='relative flex flex-row items-end justify-end'>
                   <span>
                     <svg className="items-center w-4 h-6 sm:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" /></svg>
                   </span>
                   Contacto 
                   <span>
-                    <svg className="items-center w-4 h-6 hidden sm:flex" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
+                    <svg className="items-center hidden w-4 h-6 sm:flex" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
                   </span>
                 </button>
-                <ItemListContainer className={`${ menu ? 'flex' : 'hidden'} absolute flex-col w-36 text-center bg-black rounded-lg menuContacto top-10`}/>
+                <ItemListContainer greeting='Hola, soy un Item' className={`${ menu ? 'flex' : 'hidden'} absolute flex-col w-36 text-center bg-black rounded-lg menuContacto top-10`}/>
               </div>
-              <CartWidget className='w-full flex justify-end sm:place-content-center'/>
+              <CartWidget className='flex justify-end w-full sm:place-content-center'/>
             </div>
             <MenuMovil onClick={toggleMenuMovil} className='flex sm:hidden'/>
         </nav>
