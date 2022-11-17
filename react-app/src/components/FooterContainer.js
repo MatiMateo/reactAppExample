4af7b104b9259a6b309b0f12ab6ee4adf5855817
Matías Scavuzzo <mati.ntvg@hotmail.com>
2022-11-17 10:14:30 -0300

    Orders solucionado y estética footer

--- react-app/src/components/FooterContainer.js
+++ react-app/src/components/FooterContainer.js
@@ -5,7 +5,7 @@ const FooterContainer = () => {
     return (
         <footer className="relative z-20 grid w-full grid-cols-1 grid-rows-2 gap-2 p-2 bg-black sm:grid-rows-1 auto-cols-auto">
             <Logo className='w-full row-start-1 p-2 place-content-center sm:col-span-5 sm:col-start-1' />
-            <ContactoFooter className='relative z-0 flex items-center justify-center w-full row-start-2 p-2 sm:col-span-6 sm:col-start-6 sm:justify-center' />
+            <ContactoFooter className='relative z-0 flex items-center justify-center w-full row-start-2 p-2 sm:row-start-1 sm:col-span-6 sm:col-start-6' />
         </footer>
     )
 }
